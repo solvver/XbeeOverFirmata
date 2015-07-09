5278dec4a2eb321a03287d54cc498726f51d378e
Arturete <arturopg91@gmail.com>
2015-07-09 08:52:24 +0200

    tfg

--- test/pushToSolvview.js
+++ test/pushToSolvview.js
@@ -2,15 +2,15 @@
  * Created by root on 17/02/15.
  */
 var firmata=require("../firmata");
-var https = require('http');
+var https = require('https');
 var url=require("url");
 var options = {
     hostname: 'demo.solvview.com',
     //hostname: '54.246.122.160',
    // port: 443,
    // path: '/api/back/post/channels',
-    port: 3000,
-    path: '/post/channels',
+    port: 443,
+    path: '/api/back/online/syscom/post/channels/1',
     method: 'POST',
     headers:{
         authentication:'{"uuid":"daq_ohlconcesiones_1","token":"RJ3FokddHWN7izYs"}'
@@ -32,7 +32,7 @@ function buildSolvviewDataFrame() {
         h: {mId: "arturete"},
         b: {
             m: {
-                id: "2_3_1", sP: 100, sC: 10, ts: now.getTime()
+                id: "3_1_1", sP: 100, sC: 10, ts: now.getTime()
             }, y: []
         },
         t: now
@@ -53,14 +53,54 @@ var board=new firmata.Board("/dev/ttyUSB0", function(err){
     board.setSamplingInterval(100);
     board.setFirmataTime();
     board.setDeliveryInterval(1000,  function(data){
-        console.log(data);
-        contReq++;
-        dataframe = buildSolvviewDataFrame();
-        dataframe.b.m.ts = (data.TS);
-         dataframe.t= new Date (data.TS);
-        for(var i=0;i<data.SC;i++) {
-            dataframe.b.y[i] = data.samples[i];
+
+        if(data.pin==0){
+
+            dataframe = buildSolvviewDataFrame();
+            dataframe.b.m.id="3_1_1"
+            dataframe.b.m.ts = (data.TS);
+            dataframe.t= new Date (data.TS);
+            for(var i=0;i<data.SC;i++) {
+                dataframe.b.y[i] = (100*(data.samples[i]+0.12))+8000;
+            }
+            console.log(dataframe);
+
+            sendCloud(dataframe);
+
         }
+        if(data.pin==1){
+            dataframe = buildSolvviewDataFrame();
+            dataframe.b.m.id="3_1_2"
+            dataframe.b.m.ts = (data.TS);
+            dataframe.t= new Date (data.TS);
+            for(var i=0;i<data.SC;i++) {
+                dataframe.b.y[i] = (100*(data.samples[i]+0.12))+8000;
+            }
+            sendCloud(dataframe);
+
+        }
+        if(data.pin==2){
+            dataframe = buildSolvviewDataFrame();
+            dataframe.b.m.id="3_1_3"
+            dataframe.b.m.ts = (data.TS);
+            dataframe.t= new Date (data.TS);
+            for(var i=0;i<data.SC;i++) {
+                dataframe.b.y[i] = data.samples[i]*100;
+            }
+            sendCloud(dataframe);
+
+        }
+        if(data.pin==3){
+            dataframe = buildSolvviewDataFrame();
+            dataframe.b.m.id="3_1_4"
+            dataframe.b.m.ts = (data.TS);
+            dataframe.t= new Date (data.TS);
+            for(var i=0;i<data.SC;i++) {
+                dataframe.b.y[i] = data.samples[i]*10;
+            }
+            sendCloud(dataframe);
+        }
+
         sendCloud(dataframe);
      });
     }, 1000);
